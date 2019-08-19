f9d5d7d9d0adb7cd6dbb7ba8f0d28a74b39e839a
ZborovskiyKyryl <speak.zborovskiy@gmail.com>
2019-08-19 21:03:47 +0300

    added method Open Google

---
+++ first-projectHomeWork/src/test/java/com/telran/selenium/OpenGoogleTest.java
@@ -0,0 +1,24 @@
+package com.telran.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class OpenGoogleTest {
+    WebDriver driver;
+    @Test
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.get("https://google.com");
+        driver.findElement(By.name("q")).sendKeys("java");
+        //driver.navigate().to();
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.quit();
+    }
+
+}
